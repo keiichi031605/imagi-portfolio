5b40810711e1b7cc08f4420731544a16151e9675
keiichi031605 <keichi031605@gmail.com>
2022-01-27 07:34:35 +1100

    Set up landing page and header

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,29 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <Header />
+    <router-view />
   </div>
 </template>
 
 <script>
+import Header from './components/Header'
+// import Footer from './components/Footer'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Header
+    // Footer
+  }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: rgb(52, 58, 64);
 }
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div class="main-image">
+      <h1 class="main-image-title mb-5">
+        Imagi Kitano
+      </h1>
+      <p style="color: black">
+        Hello, my name is Imagi based in Japan. A water colour artist, enjoying to draw nature by inspired Japanese animism.
+      </p>
+    </div>
+  </div>
+</template>
+
+<script></script>
+
+<style scoped>
+.main-image {
+  background-image: url(../assets/home.jpeg);
+  height: 100vh
+}
+.main-image-title {
+  font-size: 70px;
+  padding-top: 250px;
+}
+
+@media screen and (max-width: 800px) {
+  .main-image-title {
+    font-size: 50px;
+    padding-top: 150px;
+  }
+}
+</style>
